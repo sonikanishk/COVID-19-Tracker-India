6ad6edd7f11bd3e135ff7b2df5cb7c07f7796223
Kanishk Soni <58775605+sonikanishk@users.noreply.github.com>
2020-06-21 18:47:08 +0530

    Footer Fixed
    Signed-off-by: Kanishk Soni <58775605+sonikanishk@users.noreply.github.com>

--- src/Head.js
+++ src/Head.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactCountryFlag from "react-country-flag";
-
+import './Head.css';
 class Head extends React.Component {
     render(){
         return(
