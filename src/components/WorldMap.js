e523b03561e0fe56084f9896cf99e1b1552658a9
Kanishk Soni <58775605+sonikanishk@users.noreply.github.com>
2020-06-29 02:55:46 +0530

    Map Fixed
    Signed-off-by: Kanishk Soni <58775605+sonikanishk@users.noreply.github.com>

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -2,40 +2,41 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Chart } from "react-google-charts";
 
+
 export default function StateTable(props) {
-    const data = [
-        ["Country Code","Country","Total Cases"],
+      var data = [
+        ["Country Code","Total Cases"],
       ];
       const Countries = props.Countries;
       Countries.map(country=>{
-        const pair = [country.CountryCode,country.Country,country.TotalConfirmed];
+        var pair = [country.CountryCode,country.TotalConfirmed];
         return(
               data.push(pair)
           )
       })
 return (
     <div style={{textAlign: "center",paddingTop: "10rem",paddingBottom: "10rem" }}>
-        <div className="App">
+        <div>
         <Chart
-          chartEvents={[
-            {
-              eventName: "select",
-              callback: ({ chartWrapper }) => {
-                const chart = chartWrapper.getChart();
-                const selection = chart.getSelection();
-                if (selection.length === 0) return;
-                // const region = data[selection[0].row + 1];
-                // console.log("Selected : " + region);
-              }
-            }
-          ]}
+          // chartEvents={[
+          //   {
+          //     eventName: "select",
+          //     callback: ({ chartWrapper }) => {
+          //       const chart = chartWrapper.getChart();
+          //       const selection = chart.getSelection();
+          //       if (selection.length === 0) return;
+          //       // const region = data[selection[0].row + 1];
+          //       // console.log("Selected : " + region);
+          //     }
+          //   }
+          // ]}
           chartType="GeoChart"
           width="100%"
           height="600px"
           options = {{
-            // region: 'IT',
-            // displayMode: 'markers',
-            // colorAxis: {colors: ['#FFF9C4','#FF8080',]}
+            //region: 'IN',
+            displayMode: 'auto',
+            
             colorAxis: {colors: ['white','orange','orange','red']}
 
           }}
@@ -47,3 +48,4 @@ return (
     
   );
 }
+
