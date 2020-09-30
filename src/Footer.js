f4d6996e7c3dd7f5237c3ab504ab7fa384fcab34
Shreya Raj <41835298+yay14@users.noreply.github.com>
2020-10-01 01:37:58 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -13,11 +13,11 @@ class Footer extends React.Component {
                             <div>
                                 <a href="https://www.linkedin.com/in/kanishk-soni-282342197/" className="fab fa-linkedin icon" >{}</a>
                                 <a href="https://github.com/sonikanishk" className="fa fa-github icon">{}</a>
-                                <a href="mailto:soni.kanishk710@gmail.com" className="fas fa-envelope icon">{}</a>
+                                <a href="e-mail:soni.kanishk710@gmail.com" className="fas fa-envelope icon">{}</a>
                             </div>
                         </div>
                         <div className="col-12 copyright">
-                            <span> Copyright © 2020 Kanishk Soni </span> 
+                            <span> Copyright ©  2020  Kanishk Soni </span> 
                         </div>
 
                     </div>
